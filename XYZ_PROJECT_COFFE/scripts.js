b5d9afba7342b9173dbf84a021e3c96c6548fad1
SrLLamaDev <srllamadev@gmail.com>
2025-07-22 11:21:35 -0400

    First part documentation // ?Menú móvil

--- XYZ_PROJECT_COFFE/scripts.js
+++ XYZ_PROJECT_COFFE/scripts.js
@@ -2,6 +2,15 @@
 document.querySelector('.mobile-menu').addEventListener('click', function() {
     document.querySelector('.nav-links').classList.toggle('active');
 });
+/*mini documentation*/
+//! document.querySelector('.mobile-menu'): 
+//--> Selecciona el primer elemento con la clase mobile-menu.
+//! .addEventListener('click', function() {...}): 
+//--> Añade un evento que se activa al hacer clic en el menú móvil.
+//! classList.toggle('active'): 
+//--> Al hacer clic, cambia el estado de la clase active en el elemento con nav-links, mostrando o escondiendo el menú.
+
+
 
 // Scroll suave
 document.querySelectorAll('a[href^="#"]').forEach(anchor => {
